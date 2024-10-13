07450db10764fcaba1c5b87846bee7b920a69206
Flame <5017_30@mail.ru>
2024-10-13 14:56:46 +0300

    fix: image upload (yandex disk)

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -2,30 +2,45 @@ import {
 	BadRequestException,
 	Controller,
 	Post,
-	Req,
 	UploadedFile,
 	UseInterceptors
 } from '@nestjs/common'
 import { ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger'
 import { FileInterceptor } from '@nestjs/platform-express'
-import { Request } from 'express'
-import { imagesStorage } from '../storage'
+import * as axios from 'axios'
+import * as process from 'node:process'
+import { v4 as uuidv4 } from 'uuid' // Import UUID
 
 @ApiTags('upload')
 @Controller('upload')
 export class UploadController {
+	private readonly YANDEX_API_BASE_URL =
+		'https://cloud-api.yandex.net/v1/disk/resources'
+	private readonly OAUTH_TOKEN = process.env.OAUTH_TOKEN
+
+	private getAuthHeaders() {
+		return {
+			Authorization: `OAuth ${this.OAUTH_TOKEN}`
+		}
+	}
+
+	private async getYandexDiskUrl(endpoint: string, path: string) {
+		const response = await axios.default.get(
+			`${this.YANDEX_API_BASE_URL}/${endpoint}?path=${encodeURIComponent(path)}`,
+			{ headers: this.getAuthHeaders() }
+		)
+		return response.data.href
+	}
+
 	@Post('image')
 	@UseInterceptors(
 		FileInterceptor('image', {
-			storage: imagesStorage,
+			limits: { fileSize: 5 * 1024 * 1024 },
 			fileFilter: (_, file, cb) => {
 				if (!file.mimetype.match(/\/(jpg|jpeg|png)$/)) {
-					cb(new BadRequestException('Unsupported file type'), false)
+					return cb(new BadRequestException('Unsupported file type'), false)
 				}
 				cb(null, true)
-			},
-			limits: {
-				fileSize: 5 * 1024 * 1024
 			}
 		})
 	)
@@ -41,15 +56,27 @@ export class UploadController {
 			}
 		}
 	})
-	async create(
-		@UploadedFile() image: Express.Multer.File,
-		@Req() req: Request
-	) {
+	async create(@UploadedFile() image: Express.Multer.File) {
 		if (!image) {
 			throw new BadRequestException('File upload failed')
 		}
 
-		const fileUrl = `${req.protocol}://${req.get('host')}/uploads/images/${image.filename}`
+		const filePath = `${uuidv4()}_${image.originalname}`
+
+		const uploadUrl = await this.getYandexDiskUrl('upload', filePath)
+
+		await axios.default.put(uploadUrl, image.buffer, {
+			headers: { 'Content-Type': 'application/octet-stream' }
+		})
+
+		await axios.default.put(
+			`${this.YANDEX_API_BASE_URL}/publish?path=${encodeURIComponent(filePath)}`,
+			null,
+			{ headers: this.getAuthHeaders() }
+		)
+
+		const fileUrl = await this.getYandexDiskUrl('download', filePath)
+
 		return { url: fileUrl }
 	}
 }
